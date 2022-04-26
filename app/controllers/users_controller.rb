ecfbe955813ebc978b520d917c62f9ae74018c23
valentinavolgina2 <64236396+valentinavolgina2@users.noreply.github.com>
2022-04-26 11:37:04 -0700

    Added edit profile form & some validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,49 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.new(user_params)
 
-    user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
 
-    session[:user_id] = user.id
+      redirect_to root_path, notice: 'You have registered on AskMe!'
+    else
+      flash.now[:alert] = 'Some of the fields are incorrect!'
 
-    redirect_to root_path, notice: 'You have registered on AskMe!'
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'Your profile data has been updated!'
+    else
+      flash.now[:alert] = 'An error occurred during saving!'
+
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    session.delete(:user_id)
+
+    redirect_to root_path, notice: 'User has been deleted!'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name, :nickname, :email, :password, :password_confirmation
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   resource :session, only: %i[new create destroy]
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update destroy]
 
   root to: 'questions#index'
 end
