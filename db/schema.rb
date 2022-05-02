b82caf98a9032ff0faa398ecd9b82ee46870f0af
valentinavolgina2 <64236396+valentinavolgina2@users.noreply.github.com>
2022-05-02 08:35:17 -0700

    Added email index to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_02_145223) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_02_152735) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_02_145223) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email"
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
