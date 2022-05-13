4f7a2318e303d760a55a6561b67c2849bbef24ba
valentinavolgina2 <64236396+valentinavolgina2@users.noreply.github.com>
2022-05-13 10:24:25 -0700

    Added hashtag models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_03_024700) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_13_171729) do
+  create_table "hashtags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "question_hashtags", force: :cascade do |t|
+    t.integer "question_id", null: false
+    t.integer "hashtag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hashtag_id"], name: "index_question_hashtags_on_hashtag_id"
+    t.index ["question_id"], name: "index_question_hashtags_on_question_id"
+  end
+
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -42,5 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_03_024700) do
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
+  add_foreign_key "question_hashtags", "hashtags"
+  add_foreign_key "question_hashtags", "questions"
   add_foreign_key "questions", "users", column: "author_id"
 end
