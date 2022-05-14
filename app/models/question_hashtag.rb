7b1879f703ea466c883536ef0a28330449f1fdf6
valentinavolgina2 <64236396+valentinavolgina2@users.noreply.github.com>
2022-05-13 19:36:15 -0700

    Moved hashtag present to question class

--- app/models/question_hashtag.rb
+++ app/models/question_hashtag.rb
@@ -1,8 +1,4 @@
 class QuestionHashtag < ApplicationRecord
   belongs_to :question
   belongs_to :hashtag
-
-  def self.tag_present(question_id, tag_name)
-    joins(:question, :hashtag).where(question_id: question_id).where('hashtags.name': tag_name).any?
-  end
 end
