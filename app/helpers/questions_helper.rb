d9de0fbea7bacec0deeee7a9afb257a71472fe7f
valentinavolgina2 <64236396+valentinavolgina2@users.noreply.github.com>
2022-05-13 21:57:04 -0700

    Added hashtag links to question body and answer

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,7 @@
 module QuestionsHelper
+  VALID_HASHTAG_REGEX = /#[[:word:]-]+/
+
+  def replace_tags_with_links(text)
+    text.gsub(VALID_HASHTAG_REGEX) { |tag| link_to tag, search_questions_path(tag: tag.sub('#','')) }
+  end
 end
