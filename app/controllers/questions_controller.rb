d2e20067c8b1635ccae8f9fdf78d2cf2d783738f
valentinavolgina2 <valentina.volgina2@gmail.com>
2022-05-19 21:19:06 -0700

    Changed questions to question_hashtags in where exists

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -85,7 +85,7 @@ class QuestionsController < ApplicationController
   end
 
   def set_hashtags
-    @hashtags = Hashtag.where_exists(:questions).order(:name).pluck(:name)
+    @hashtags = Hashtag.where_exists(:question_hashtags).order(:name).pluck(:name)
   end
 
   def check_captcha(model)
